502beed0686be398551e16c0beb3794c26db385b
Chris Lo Coco <chris.lococo@webfilings.com>
2013-08-27 01:25:27 -0600

    Continued to expand on the data storage models.
    Added in the basic units of skills, which will be a list of reference
    properties inside the character rather than a whole bunch of extra data
    points floating off disconnected except with KeyProperties.

--- Models/account.py
+++ Models/account.py
@@ -1,1 +1,9 @@
 __author__ = 'chrislococo'
+
+from google.appengine.ext import ndb
+
+class Player(ndb):
+    user = ndb.UserProperty()
+    email = ndb.StringProperty()
+    last_login = ndb.DateTimeProperty()
+    character = ndb.KeyProperty(kind="Character", repeated=True, indexed=True)
--- Models/campaign.py
+++ Models/campaign.py
@@ -1,1 +1,11 @@
 __author__ = 'chrislococo'
+
+from google.appengine.ext import ndb
+
+
+class Campaign(ndb):
+    start_date = ndb.DateProperty(indexed=False, auto_now_add=True)
+    last_played = ndb.DateTimeProperty(indexed=True, auto_now=True)
+    game_master = ndb.KeyProperty(kind="Account", indexed=True, required=True)
+    players = ndb.KeyProperty(kind="Account", indexed=True, repeated=True)
+
